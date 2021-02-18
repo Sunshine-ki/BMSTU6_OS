8b4df79e6ee150a5280a77bdb67a191d8b74a623
Sunshine-ki <sukocheva.alis@mail.ru>
2021-02-18 19:00:52 +0300

    New lab

---
+++ lab_01/catalog/headers/path.h
@@ -0,0 +1,22 @@
+#ifndef _PATH_H_
+
+#define _PATH_H_
+
+typedef struct
+{
+	int count;
+	char **path;
+	int depth;
+} Path;
+
+Path *CreatePath();
+
+void DestroyPath(Path *path);
+
+void Push(Path *path, char *elem);
+
+char *Pop(Path *path);
+
+void Output(const Path const * const path);
+
+#endif
