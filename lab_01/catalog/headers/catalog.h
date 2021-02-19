e2f8ca1c5292c71c8cb13dcf896aa9c2de33c03f
Sunshine-ki <sukocheva.alis@mail.ru>
2021-02-19 10:49:22 +0300

    Update

--- lab_01/catalog/headers/catalog.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CATALOG_H_
-
-#define _CATALOG_H_
-
-void catalog(char *pathname);
-
-#endif
--- lab_01/catalog/headers/errors.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ERRORS_H_
-
-#define _ERRORS_H_
-
-void err_sys(const char *x);
-
-void error_lstat();
-
-#endif
