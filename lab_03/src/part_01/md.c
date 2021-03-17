3e913f9e5da2e26b415661fe623ce33033e91db3
Sunshine-ki <sukocheva.alis@mail.ru>
2021-03-17 22:55:21 +0300

    ADD: part 01

---
+++ lab_03/src/part_01/md.c
@@ -0,0 +1,21 @@
+#include <linux/module.h> // MODULE_LICENSE, MODULE_AUTHOR
+#include <linux/kernel.h> // KERN_INFO
+#include <linux/init.h>	  // ​Макросы __init и ​__exit
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alice");
+MODULE_DESCRIPTION("My first module!");
+
+static int __init md_init(void)
+{
+	printk(KERN_INFO "Module: Hello world!\n");
+	return 0;
+}
+
+static void __exit md_exit(void)
+{
+	printk(KERN_INFO "Module: Good by!\n");
+}
+
+module_init(md_init);
+module_exit(md_exit);
